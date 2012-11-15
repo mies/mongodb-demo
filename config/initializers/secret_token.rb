bdcb77830a2fbdf00661bec46feb015dad117382
Micha Hernandez van Leuffen <mies@distributedaweso.me>
2012-11-15 13:59:15 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongodbDemo::Application.config.secret_token = '84df0ad4b0ab9e332fcca237ca12a58fa33e14ffd9c7349c1e3e37593e5c5ebcf92b610a49b065081e62326acb4d26aead4a7b28a8df42f056cb81e6a3484afc'
